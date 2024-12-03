5f413559e3456cd75b67b8bd8ec6760df3573449
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2024-12-02 22:54:48 -0500

    Day 1 part 2 completed

--- src/aoc2024/Day1.java
+++ src/aoc2024/Day1.java
@@ -3,11 +3,34 @@ package aoc2024;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Day1 {
-    public int part2(ArrayList<String> input) {
-        return 1;
+    public static int part2(ArrayList<String> input) {
+        ArrayList<Integer> leftList = new ArrayList<>();
+        HashMap<Integer, Integer> rightFreq = new HashMap<>();
+
+        for (String line: input) {
+            String[] numbers = line.split(" {3}");
+            leftList.add(Integer.valueOf(numbers[0]));
+
+            if (rightFreq.containsKey(Integer.valueOf(numbers[1]))) {
+                int n = rightFreq.get(Integer.valueOf(numbers[1]));
+                rightFreq.put(Integer.valueOf(numbers[1]), n + 1);
+            } else {
+                rightFreq.put(Integer.valueOf(numbers[1]), 1);
+            }
+        }
+
+        int result = 0;
+        for (Integer n: leftList) {
+            if (rightFreq.containsKey(n)) {
+                result += n * rightFreq.get(n);
+            }
+        }
+
+        return result;
     }
 
     public static int part1(ArrayList<String> input) {
@@ -47,5 +70,6 @@ public class Day1 {
         }
 
         System.out.println(part1(input));
+        System.out.println(part2(input));
     }
 }
